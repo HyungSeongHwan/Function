476fdc5751d64b9f3fafe7fb0f7d3cc5ebb4f79b
SeungHwan <a77285952@gmail.com>
2024-11-29 19:04:20 +0900

    FunctionTest

---
+++ FunctionTest/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FunctionTest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "실행 결과";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtResult.ForeColor = System.Drawing.Color.Green;
+            this.txtResult.Location = new System.Drawing.Point(36, 82);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(574, 330);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "결과값";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnOK.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnOK.Location = new System.Drawing.Point(145, 469);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(147, 45);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Green;
+            this.btnCancel.Location = new System.Drawing.Point(337, 469);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(147, 45);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Clear";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 549);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(8);
+            this.Name = "frmMain";
+            this.Text = "FunctionTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ FunctionTest/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunctionTest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            TestFunction1();
+            TestFunctuon2();
+            TestIf();
+            TestSwitch();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+
+        public void TestFunction1()
+        {
+            int nSum = Sum(10, 20);
+            string sResult = string.Format("Sum = {0}", nSum);
+            Console.WriteLine(sResult);
+
+            txtResult.Text = sResult + "\n";
+
+            txtResult.Text += "~~~~~~~~~~~~~~~~~~~~~~";
+        }
+
+        public void TestFunctuon2()
+        {
+            int a = 100;
+            int b = 200;
+            string sResult = "";
+
+            sResult = string.Format("a = {0}, b = {1}", a, b);
+            txtResult.Text += sResult + "\n";
+
+            SwapTest(a, b);
+            sResult = string.Format("a = {0}, b = {1}", a, b);
+            txtResult.Text += sResult + "\n";
+
+            Swap(ref a, ref b);
+            sResult = string.Format("a = {0}, b = {1}", a, b);
+            txtResult.Text += sResult + "\n";
+
+            txtResult.Text += "~~~~~~~~~~~~~~~~~~~~~~";
+        }
+
+        public int Sum(int a, int b)
+        {
+            return (a + b);
+        }
+
+        public void SwapTest(int a, int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        public void Swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        public void TestIf()
+        {
+            string sResult = "";
+            int a = 10;
+            int b = 20;
+
+            if (b > a)
+                sResult = "b 가 a 보다 큽니다.";
+            else if (a > b)
+                sResult = "a 가 b 보다 큽니다.";
+            else
+                sResult = "a 와 b 는 같다.";
+
+            txtResult.Text += sResult + "\n";
+            txtResult.Text += "~~~~~~~~~~~~~~~~~~~~~~";
+        }
+
+        public void TestSwitch()
+        {
+            string sResult = "";
+            const int apple = 1;
+            const int orange = 2;
+            const int banana = 3;
+            int a = orange;
+            switch (a)
+            {
+                case apple:
+                    sResult = "사과입니다.";
+                    break;
+                case orange:
+                    sResult = "오렌지입니다.";
+                    break;
+                case banana:
+                    sResult = "바나나입니다.";
+                    break;
+                default:
+                    break;
+            }
+
+            txtResult.Text += sResult + "\n";
+            txtResult.Text += "~~~~~~~~~~~~~~~~~~~~~~";
+        }
+    }
+}
